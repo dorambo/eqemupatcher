ac9b102bec867acb47e20c55ce848842737288d9
dorambo <119876446+dorambo@users.noreply.github.com>
2026-06-25 08:03:24 -0500

    update patcher

--- EQEmu Patcher/build.bat
+++ EQEmu Patcher/build.bat
@@ -1,8 +1,8 @@
 echo building....
 set VERSION=0.0.0.1
 set BUILD_CONFIGURATION=Release
-set SERVER_NAME=EQEMU Patcher
-set FILE_NAME=emupatcher
-set FILELIST_URL=https://github.com/xackery/eqemupatcher/releases/latest/download
-set PATCHER_URL=https://github.com/xackery/eqemupatcher/releases/latest/download
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\msbuild.exe" /m /p:Configuration=%BUILD_CONFIGURATION% /p:VERSION=%VERSION% /p:SERVER_NAME="%SERVER_NAME%" /p:FILELIST_URL="%FILELIST_URL%" /p:PATCHER_URL="%PATCHER_URL%" /p:FILE_NAME="%FILE_NAME%"
+set SERVER_NAME=The Hero Chronicles
+set FILE_NAME=eqemupatcher
+set FILELIST_URL=https://raw.githubusercontent.com/dorambo/eqemupatcher/master
+set PATCHER_URL=https://raw.githubusercontent.com/dorambo/eqemupatcher/master/rof
+"C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe" /m /p:Configuration=%BUILD_CONFIGURATION% /p:VERSION=%VERSION% /p:SERVER_NAME="%SERVER_NAME%" /p:FILELIST_URL="%FILELIST_URL%" /p:PATCHER_URL="%PATCHER_URL%" /p:FILE_NAME="%FILE_NAME%"
